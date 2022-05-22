8e9f2a3b6e879743d2aed95f4eff49dcc102c17c
Carlos <Carlos@DESKTOP-6QCDHPK>
2022-05-22 16:38:56 -0300

    Primer commit

---
+++ TP5_Grupo7/src/main/java/ar/unju/edu/fi/Tp4Grupo7Application.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo7Application.class, args);
+	}
+
+}
---
+++ TP5_Grupo7/src/test/java/ar/unju/edu/fi/Tp4Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
